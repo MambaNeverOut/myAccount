ef4e35891a358c727438d48b36e607ecfd9a9fa5
Irelia <yaokai729@outlook.com>
2020-03-02 21:17:24 +0800

    deploy

---
+++ precache-manifest.966af81781f3eda58c66ed9249b15d83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e8e4ebf90fd68f9ac02",
+    "url": "/myAccount/css/app.d86c7915.css"
+  },
+  {
+    "revision": "cd6d4a4daf9dc236d7baff9c90e02b8e",
+    "url": "/myAccount/index.html"
+  },
+  {
+    "revision": "8e8e4ebf90fd68f9ac02",
+    "url": "/myAccount/js/app.a098eefe.js"
+  },
+  {
+    "revision": "8b7ee468f6af4205fc26",
+    "url": "/myAccount/js/chunk-vendors.1aa09ae5.js"
+  },
+  {
+    "revision": "f0fce8a1d94de3ce6d9839c05c88d85e",
+    "url": "/myAccount/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/myAccount/robots.txt"
+  }
+]);
